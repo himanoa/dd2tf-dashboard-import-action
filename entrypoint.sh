90af912e402c306a4731a3c538a245d45f8310b5
himanoa <matsunoappy@gmail.com>
2019-12-17 15:27:35 +0900

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-output=`$GOPATH/src/github.com/amnk/dd2tf/dd2tf $INPUT_SUBCOMMAND $INPUT_OPTION`
+output=`DATADOG_API_KEY=$INPUT_DATADOG_API_KEY DATADOG_APP_KEY=$INPUT_DATADOG_APP_KEY $GOPATH/src/github.com/amnk/dd2tf/dd2tf $INPUT_SUBCOMMAND $INPUT_OPTION`
 echo ::set-output name=terraform::$output
